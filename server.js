0d7049cc2cac944c9a8fd5759dbc7091d9547f02
Dagmar Cirino <dagmar.mv@gmail.com>
2018-02-16 00:57:14 -0200

    Added mongodb and express. Config server node for api request

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/api/mensagem', (req, res) => {
+  res.send({ express: 'É nóis mano. API funcionando perfeitamente :) HAHAHA' });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
