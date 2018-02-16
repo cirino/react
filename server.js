ff1f8de6918a3f1f4feb63efb683485d0ec88946
Dagmar Cirino <dagmar.mv@gmail.com>
2018-02-16 03:30:50 -0200

    Correção em server node

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const port = process.env.PORT || 5000;
 
-app.use('*', express.static(path.join(__dirname, '/client/public')))
+app.use('/', express.static(path.join(__dirname, '/client/public')))
 
 app.get('/api/mensagem', (req, res) => {
   res.send({ express: 'É nóis mano. API funcionando perfeitamente :) HAHAHA' });
