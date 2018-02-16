d870ef396397a5e7645061f91c84aae73a1168b0
Dagmar Cirino <dagmar.mv@gmail.com>
2018-02-16 02:43:24 -0200

    Correção em server node

--- server.js
+++ server.js
@@ -20,22 +20,22 @@ const assert = require('assert');
 const url = 'mongodb://localhost:27017';
 const dbName = 'versaobeta';
 
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Conectou no servidor mongodb");
+ 
+  const db = client.db(dbName);
+ 
+  client.close();
+});
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/api/mensagem', (request, response) => {
+app.get('*', (request, response) => {
   response.send({express: 'É nóis mano. API funcionando perfeitamente :) HAHAHA!'})
 });
 
 app.listen(app.get('port'), () => {
   console.log("Rodando em localhost: " + app.get('port'))
 })
-
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Não conectou no servidor mongodb");
- 
-  const db = client.db(dbName);
- 
-  client.close();
-});
