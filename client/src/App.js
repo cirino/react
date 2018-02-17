df24db8d41461cf089da88b671676a57f9a0a4f1
Dagmar Cirino <dagmar.mv@gmail.com>
2018-02-17 01:57:31 -0200

    Correção do server. Adicionado Express e configuração de Api funcional

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,64 @@
+/*
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: null,
+      fetching: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`status ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(json => {
+        this.setState({
+          message: json.message,
+          fetching: false
+        });
+      }).catch(e => {
+        this.setState({
+          message: `API call failed: ${e}`,
+          fetching: false
+        });
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          {'This is '}
+          <a href="https://github.com/mars/heroku-cra-node">
+            {'create-react-app with a custom Node/Express server'}
+          </a><br/>
+        </p>
+        <p className="App-intro">
+          {this.state.fetching
+            ? 'Fetching message from API'
+            : this.state.message}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
+*/
+
 import React, { Component } from 'react'
 import './App.css'
 import Categorias from './modulos/Categoria'
@@ -12,7 +73,9 @@ class App extends Component {
     
     this.state = {
       cat: 1,
-      response: ''
+      response: '',
+      message: null,
+      fetching: true
     }
 
     this.handleClickCategorias = this.handleClickCategorias.bind(this);
@@ -20,19 +83,38 @@ class App extends Component {
   }
 
     componentDidMount() {
-      this.callApi()
-        .then(res => this.setState({ response: res.express }))
-        .catch(err => console.log(err));
+      fetch('/api/mensagem')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`status ${response.status}`);
+        }
+        return response.json();
+        console.log('carregou');
+      })
+      .then(json => {
+        this.setState({
+          message: json.message,
+          fetching: false
+        })
+        console.log('json');
+      }).catch(e => {
+        this.setState({
+          message: `API call failed: ${e}`,
+          fetching: false
+        })        
+        console.log('erro');
+      })
     }
 
+    /*
     callApi = async () => {
       const response = await fetch('/api/mensagem');
       const body = await response.json();
       if (response.status !== 200) throw Error(body.message);
-
+      console.log('status http: ' + response.status)
       return body;
     };
-
+    */
 
     filho = ( ) => { 
       alert('filho')
@@ -61,7 +143,11 @@ class App extends Component {
           <div className="container-box">
             <header className="box">
               <h1 className="tituloLogo">CPIX Design - Tabela de nutrientes alimentares completa</h1>
-              <p className="App-intro">{this.state.response}</p>
+              <p className="App-intro">
+                {this.state.fetching
+                  ? 'Recuperando dados da API'
+                  : this.state.message}
+                </p>
             </header>
 
             <aside className="box">
--- client/src/index.js
+++ client/src/index.js
@@ -9,3 +9,16 @@ render(
     document.querySelector('[data-js="App"]')
 )
 registerServiceWorker();
+
+
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+*/
