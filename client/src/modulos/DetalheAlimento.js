63f02efe28b3d1aa7723b9f2986873077e585ade
Dagmar Cirino <dagmar.mv@gmail.com>
2018-02-18 02:31:34 -0300

    Correção de bugs e adicionado reactstrap

--- client/src/modulos/DetalheAlimento.js
+++ client/src/modulos/DetalheAlimento.js
@@ -6,13 +6,13 @@ const DetalheAlimento = ( props ) => (
     
     <Container>
         <Row>
-            <Col>energia kcal: ${props.energia.kcal}</Col>
-            <Col>carboidrato: ${props.carboidrato}</Col>
-            <Col>colesterol: ${props.colesterol}</Col>
-            <Col>ferro: ${props.ferro}</Col>
-            <Col>proteina: ${props.proteina}</Col>
-            <Col>sódio:  ${props.sodio}</Col>
-            <Col>umidade: ${props.umidade}</Col>
+            <Col>energia kcal: {props.energia.kcal}</Col>
+            <Col>carboidrato: {props.carboidrato}</Col>
+            <Col>colesterol: {props.colesterol}</Col>
+            <Col>ferro: {props.ferro}</Col>
+            <Col>proteina: {props.proteina}</Col>
+            <Col>sódio:  {props.sodio}</Col>
+            <Col>umidade: {props.umidade}</Col>
         </Row>
     </Container>
 )
